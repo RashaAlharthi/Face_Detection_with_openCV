a6805619e8b1de3d5647e6c8aa6bc02e1c18bb0e
RashaAlharthi <81419024+RashaAlharthi@users.noreply.github.com>
2021-07-16 07:56:11 +0300

    Update realTimeFaceDetection.py

--- realTimeFaceDetection.py
+++ realTimeFaceDetection.py
@@ -5,21 +5,21 @@
 
 import cv2
 
-
+# Load the cascade
 face_detection = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 video = cv2.VideoCapture(0)
 
 while True:
+    # Read input video & Detect faces
     check, frame = video.read()
     faces = face_detection.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5)
-    
+    # Draw rectangle around the faces
     for x,y,w,h in faces:
         frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
-        
+     # Open (Face Detector) window to show the output   
     cv2.imshow('Face Detector', frame)
-    
     key = cv2.waitKey(1)
-    
+    # Stop the run
     if key == ord('q'):
         beake
 
